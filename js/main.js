9fd4250027a18ca4e7dd3152f874b5d9f5693d5d
WreckitTR <Wreckittr@gmail.com>
2020-04-18 15:24:09 -0500

    adding js files

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import ExampleWork from './example-work.js';
+
+ReactDom.render(<ExampleWork />, document.getElementById('example-work'));
